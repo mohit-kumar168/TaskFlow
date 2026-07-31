e4f2154cbb8d8f6450460477e4258d8a94361b44
Mohit Kumar <159265862+mohit-kumar168@users.noreply.github.com>
2026-07-31 14:01:48 +0530

    feat(workspace-members): implement workspace member management

--- backend/src/routes/workspace.routes.ts
+++ backend/src/routes/workspace.routes.ts
@@ -1,4 +1,5 @@
 import { createWorkspace, deleteWorkspace, fetchAllWorkspaces, fetchWorkspace } from "@/controllers/workspace.controller";
+import { addMember, fetchAllMembers, removeMember, updateMemberRole } from "@/controllers/workspaceMember.controller";
 import { protect } from "@/middleware/auth.middleware";
 import { Router } from "express";
 
@@ -10,5 +11,10 @@ router.post("/", createWorkspace);
 router.get("/", fetchAllWorkspaces);
 router.get("/:workspaceId", fetchWorkspace);
 router.delete("/:workspaceId", deleteWorkspace);
+
+router.post("/:workspaceId/members", addMember);
+router.get("/:workspaceId/members", fetchAllMembers);
+router.patch("/:workspaceId/members/:memberId", updateMemberRole);
+router.delete("/:workspaceId/members/:memberId", removeMember);
 
 export default router;
