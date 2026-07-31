884c35a2d44e9625731849e1ec755134e511cccf
Mohit Kumar <159265862+mohit-kumar168@users.noreply.github.com>
2026-07-31 20:16:01 +0530

    feat(project-member): implement project member management

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { protect } from "../middleware/auth.middleware"
 import { fetchAllProjects, fetchProject, removeProject, updateProject, createProject } from "@/controllers/project.controller";
+import { addMember, fetchAllProjectMembers, fetchProjectMember, removeProjectMember, updateProjectMember } from "@/controllers/projectMember.controller";
 
 const router = Router();
 
@@ -11,5 +12,11 @@ router.get("/:workspaceId/projects", fetchAllProjects);
 router.get("/:workspaceId/projects/:projectId", fetchProject);
 router.patch("/:workspaceId/projects/:projectId", updateProject);
 router.delete("/:workspaceId/projects/:projectId", removeProject);
+
+router.post("/:workspaceId/projects/:projectId/members", addMember);
+router.get("/:workspaceId/projects/:projectId/members", fetchAllProjectMembers);
+router.get("/:workspaceId/projects/:projectId/members/:memberId", fetchProjectMember);
+router.patch("/:workspaceId/projects/:projectId/members/:memberId", updateProjectMember);
+router.delete("/:workspaceId/projects/:projectId/members/:memberId", removeProjectMember);
 
 export default router;
